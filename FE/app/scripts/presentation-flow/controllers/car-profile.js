8933fd30b480d0cdf0181856a885ace9cc4f3934
Rammel Maestre <rammel.maestre@globant.com>
2014-11-11 11:53:28 -0200

    Complete recalls information

--- FE/app/scripts/presentation-flow/controllers/car-profile.js
+++ FE/app/scripts/presentation-flow/controllers/car-profile.js
@@ -1,13 +1,31 @@
 'use strict';
 
-angular.module('PresentationFlow').controller('CarProfileCtrl', function ($scope, RedirectSrv) {
+angular.module('PresentationFlow').controller('CarProfileCtrl', function ($scope, RedirectSrv, RecallsSrv) {
 
-    $scope.searchStore = function(storeZipCode){
-        RedirectSrv.redirectTo('/store-locator?zipcode='+(storeZipCode || ''));
+    $scope.searchStore = function (storeZipCode) {
+        RedirectSrv.redirectTo('/store-locator?zipcode=' + (storeZipCode || ''));
     };
-    
-    $scope.viewRecalls = function(){
+
+    $scope.viewRecalls = function () {
         RedirectSrv.redirectTo('/recalls');
     };
+
+    $scope.model = {
+        lastRecall: false
+    };
+
+    var lastRecallResultSuccess = function (response) {
+        $scope.model.lastRecall = response.data || false;
+    };
+
+    var lastRecallResultFaild = function (response) {
+        console.log('ERROR: ' + response);
+    };
+
+    var modelYear = '2000',
+            make = 'ITASCA',
+            model = 'HORIZON';
+
+    RecallsSrv.getLastRecall(modelYear, make, model, lastRecallResultSuccess, lastRecallResultFaild);
 
 });
--- FE/app/scripts/presentation-flow/services/recalls.js
+++ FE/app/scripts/presentation-flow/services/recalls.js
@@ -3,16 +3,20 @@
 angular.module('PresentationFlow').service('RecallsSrv', function (ApiHttpSrv, config) {
 
     var getRecalls = function (year, make, model, successCallback, faildCallback) {
-        
-        config.api.hosts.BACKEND = 'http://127.0.0.1:8080/service';
-        
         ApiHttpSrv.createHttp('GET', config.api.hosts.BACKEND + '/vehicle/recalls/year/' +
                 year + '/make/' + make + '/model/' + model + '/order/desc')
                 .then(successCallback, faildCallback);
     };
 
+    var getLastRecall = function (year, make, model, successCallback, faildCallback) {
+        ApiHttpSrv.createHttp('GET', config.api.hosts.BACKEND + '/vehicle/recalls/year/' +
+                year + '/make/' + make + '/model/' + model + '/last')
+                .then(successCallback, faildCallback);
+    };
+
     return {
-        getRecalls: getRecalls
+        getRecalls: getRecalls,
+        getLastRecall: getLastRecall
     };
 
 });
