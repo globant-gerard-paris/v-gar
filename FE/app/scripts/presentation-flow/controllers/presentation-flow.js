f095be25b7421565580fda1f6eac9e894d049e26
Diego Caro <carodiego@gmail.com>
2014-11-18 22:01:28 -0300

    add styles to mobile and refactor desktop styles

--- FE/app/scripts/presentation-flow/controllers/presentation-flow.js
+++ FE/app/scripts/presentation-flow/controllers/presentation-flow.js
@@ -1,24 +1,6 @@
 'use strict';
 
-angular.module('PresentationFlow').controller('PresentationFlowCtrl', function ($scope, PresentationFlowSrv, RedirectSrv) {
-
-    //TODO remove this.
-    /*$scope.userTypeAction = function (action) {
-        PresentationFlowSrv.setUserTypeAction(action);
-        if ('FULL_USER' === action) {
-            RedirectSrv.redirectTo('/dashboard');
-        } else {
-            RedirectSrv.redirectTo('/presentation-flow-landing');
-        }
-    };*/
+angular.module('PresentationFlow').controller('PresentationFlowCtrl', function ( ) {
 
 });
 
-//Fix carrusel ui.boostrap.
-angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
-    .controller('CarouselController', ['$scope', '$timeout', '$transition', '$q', function        ($scope, $timeout, $transition, $q) {
-    }]).directive('carousel', [function() {
-        return {
-
-        }
-    }]);
