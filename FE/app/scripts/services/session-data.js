c8b74231a09382e5a12d60788d2965e8fed837c8
Jero <jero.carrizo@gmail.com>
2014-11-21 13:58:07 -0300

    FE Integration Landing complete :)

--- FE/app/scripts/services/session-data.js
+++ FE/app/scripts/services/session-data.js
@@ -2,6 +2,11 @@
 
 angular.module('Services').service('SessionDataSrv', function () {
 
+    var model = {
+        currentUser: null,
+        cachedVehicleUsers: null
+    };
+
     /**
      * Return the Current token from i-frame session.
      * @returns {string}
@@ -15,15 +20,33 @@ angular.module('Services').service('SessionDataSrv', function () {
      * @returns {string}
      */
     var getCurrentUser = function () {
-        return '1';//TODO: dummy user id until will be defined where we can take it.
+        return model.currentUser;
     };
+
     /**
-     * Return the Current familyId session.
+     * Set the current virtual garage user.
+     * @param user
+     */
+    var setCurrentUser = function (user) {
+        model.currentUser = user;
+    };
+
+    var setCachedVehicleUsers = function (vehicles) {
+        model.cachedVehicleUsers = vehicles;
+    };
+
+    var getCachedVehicleUsers = function () {
+        return model.cachedVehicleUsers;
+    };
+
+    /**
+     * FIXME: This should be removed.
      * @returns {string}
      */
     var getCurrentFamilyId = function () {
-        return '1';//TODO: dummy family id until will be defined where we can take it.
+        return '1';
     };
+
     /**
      * Return the Current TangibleId session.
      * @returns {string}
@@ -34,9 +57,12 @@ angular.module('Services').service('SessionDataSrv', function () {
 
     return {
         getCurrentUser: getCurrentUser,
+        setCurrentUser: setCurrentUser,
         getCurrentFamilyId: getCurrentFamilyId,
         getCurrentTangibleId: getCurrentTangibleId,
-        getCurrentToken: getCurrentToken
+        getCurrentToken: getCurrentToken,
+        getCachedVehicleUsers: getCachedVehicleUsers,
+        setCachedVehicleUsers: setCachedVehicleUsers
     };
 
 });
