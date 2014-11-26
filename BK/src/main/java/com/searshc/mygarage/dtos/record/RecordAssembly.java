0b04096dce8ab3240be65b44ff76a2b128782604
Rammel Maestre <rammel.maestre@globant.com>
2014-11-26 17:51:48 -0200

    Add TODO and rename SQL script

--- BK/src/main/java/com/searshc/mygarage/dtos/record/RecordAssembly.java
+++ BK/src/main/java/com/searshc/mygarage/dtos/record/RecordAssembly.java
@@ -10,15 +10,16 @@ import com.searshc.mygarage.entities.record.Record;
  */
 public class RecordAssembly {
 
-	public static Record toRecord(final RecordDto dto) {
-		Validate.notNull(dto, "The dto object cannot be null");
-		
-		Record record = new Record();
-		record.setComment(dto.getComment());
-		record.setDate(dto.getDate());
-		//record.setService(dto.getService().getDesc());
-		record.setSource(dto.getSource());
-		record.setMileage(Integer.valueOf(dto.getMileage()));
+    public static Record toRecord(final RecordDto dto) {
+        Validate.notNull(dto, "The dto object cannot be null");
+
+        Record record = new Record();
+        record.setComment(dto.getComment());
+        record.setDate(dto.getDate());
+        //TODO: Change DTO to work with SuggestedService
+        //record.setService(dto.getService().getDesc());
+        record.setSource(dto.getSource());
+        record.setMileage(Integer.valueOf(dto.getMileage()));
 
         return record;
     }
