6500def47a2b97f603ac0da9c286e1fee546d488
Guillermo Baigorria <g.baigorria@globant.com>
2014-11-27 13:06:30 -0300

    working on carousel

--- FE/app/scripts/directives/dashboard/car-card.js
+++ FE/app/scripts/directives/dashboard/car-card.js
@@ -2,6 +2,7 @@
 
 angular.module('Directives').directive('carCard', function () {
     return {
+    	replace:true,
         restrict: 'E',
         templateUrl: 'scripts/directives/views/dashboard/car-card.html',
         scope: {
--- FE/app/scripts/presentation-flow/controllers/dashboard.js
+++ FE/app/scripts/presentation-flow/controllers/dashboard.js
@@ -1,6 +1,35 @@
 'use strict';
 
-angular.module('PresentationFlow').controller('DashboardCtrl', function ($scope, RedirectSrv) {
+
+angular.module('PresentationFlow').directive('slick', function($timeout) {
+    return function(scope, el, attrs) {
+        $timeout((function() {
+            el.slick({
+                dots: false,
+                infinite: false,
+                autoplay: false,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                responsive: [
+                    {
+                      breakpoint: 768,
+                      settings: {
+                        slidesToShow: 1
+                      }
+                    },
+                    {
+                      breakpoint: 992,
+                      settings: {
+                        slidesToShow: 2
+                      }
+                    }
+                ]
+            })
+        }), 500)
+    }
+})
+
+angular.module('PresentationFlow').controller('DashboardCtrl', function ($scope, RedirectSrv, $timeout) {
 
     $scope.redirectToCarProfile = function () {
         RedirectSrv.redirectTo('/car-profile');
@@ -14,7 +43,12 @@ angular.module('PresentationFlow').controller('DashboardCtrl', function ($scope,
         RedirectSrv.redirectTo('/car-profile?option=' + option);
     };
 
+    $timeout(function(){
+    	$scope.model.clock = {x:1};
+    },1000);
+
     $scope.model = {
+    	clock: {},
     	cars : [
     		{
 	            year: '2014',
@@ -26,6 +60,34 @@ angular.module('PresentationFlow').controller('DashboardCtrl', function ($scope,
     		{
 	            year: '2001',
 	            brand: 'Kia',
+	            model: 'Sportage',
+	            mileage: '134000',
+	            nameTag: 'The Monster'
+    		},
+  		{
+	            year: '2012',
+	            brand: 'Ford',
+	            model: 'Fiesta',
+	            mileage: '12000',
+	            nameTag: 'My Mom\'s Car'
+    		},
+    		{
+	            year: '2001',
+	            brand: 'KiaA',
+	            model: 'Sportage',
+	            mileage: '134000',
+	            nameTag: 'The Monster'
+    		},
+  		{
+	            year: '2014',
+	            brand: 'ChevroletB',
+	            model: 'Colorado',
+	            mileage: '44000',
+	            nameTag: 'My Little Luxury'
+    		},
+    		{
+	            year: '2001',
+	            brand: 'KiaB',
 	            model: 'Sportage',
 	            mileage: '134000',
 	            nameTag: 'The Monster'
