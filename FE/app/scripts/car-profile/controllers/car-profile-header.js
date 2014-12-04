ffc0f983150b4fb65706fd43c2bf7b8d589a160e
Guillermo Baigorria <g.baigorria@globant.com>
2014-12-04 12:26:23 -0300

    added car-card and mock to header

--- FE/app/scripts/car-profile/controllers/car-profile-header.js
+++ FE/app/scripts/car-profile/controllers/car-profile-header.js
@@ -1,7 +1,14 @@
 'use strict';
 
 
-angular.module('PresentationFlow').controller('CarProfileHeaderCtrl', function () {
+angular.module('PresentationFlow').controller('CarProfileHeaderCtrl', function ($scope,$http,$timeout) {
 
+	var carsResultSuccess = function(response){
+	    $scope.model.car = response.data.vehicles[0];
+	};
 
+    //MOCKK
+    $timeout( function(){
+        $http.get('resources/mocks/dashboard.json').then(carsResultSuccess);
+    },2000);
 });
