d7f699feb510cf3bb873e7e394601fbe64eeb6c9
Diego Caro <carodiego@gmail.com>
2014-12-05 12:31:22 -0300

    add mock to show info in car-profile-last-services.

--- FE/app/scripts/car-profile/controllers/car-profile-last-services.js
+++ FE/app/scripts/car-profile/controllers/car-profile-last-services.js
@@ -1,8 +1,21 @@
 'use strict';
 
 
-angular.module('PresentationFlow').controller('CarProfileLastServicesCtrl', function () {
+angular.module('PresentationFlow').controller('CarProfileLastServicesCtrl', function ($scope, $http) {
 
+    var mock = false;
 
+    var carsResultSuccess = function(response){
+        $scope.model = {
+            services : response.data.services,
+            article: response.data.article
+        };
+    };
 
+    if(mock){
+        $http.get('resources/mocks/car-profile.json').then(carsResultSuccess);
+    }
+    else{
+        //DashboardSrv.getCars(userId, carsResultSuccess, carsResultFailed);
+    }
 });
