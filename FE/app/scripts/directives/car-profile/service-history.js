48a8e898d2de15ecd62f051621a19a06e33de31f
Diego Caro <carodiego@gmail.com>
2014-12-05 10:57:24 -0300

    create directives to manage last service history.

---
+++ FE/app/scripts/directives/car-profile/service-history.js
@@ -0,0 +1,39 @@
+/**
+ * Created by diego.caro on 04/12/2014.
+ */
+'use strict';
+
+angular.module('Directives').directive('serviceHistory', function () {
+    return {
+        replace: true,
+        restrict: 'E',
+        templateUrl: 'scripts/directives/views/car-profile/service-history.html',
+        scope: {
+            serviceDate: '=',
+            serviceMileage: '=',
+            serviceCenter: '=',
+            serviceRecords: '='
+        },
+        link: function (scope) {
+
+            scope.serviceCenterStr = 'None';
+
+            if (scope.serviceCenter !== null) {
+                scope.serviceCenterStr = scope.serviceCenter.address;
+                if(scope.serviceCenter.city){
+                    scope.serviceCenterStr += ' @ ' + scope.serviceCenter.city +
+                        ', ' + scope.serviceCenter.zipCode;
+                }
+            }
+
+            var serviceDescArr = [];
+
+            _.each(scope.serviceRecords, function(record){
+                serviceDescArr.push(record.description);
+            });
+
+            scope.serviceDesc = serviceDescArr.join(', ');
+
+        }
+    };
+});
