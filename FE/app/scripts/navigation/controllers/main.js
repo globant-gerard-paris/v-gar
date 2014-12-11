a33f2629eacebb2e455b79ba5d57640153834e8e
Jero <jero.carrizo@gmail.com>
2014-12-11 18:07:34 -0300

    MYG000-197 add mobile condition.

--- FE/app/scripts/navigation/controllers/main.js
+++ FE/app/scripts/navigation/controllers/main.js
@@ -3,21 +3,21 @@
 angular.module('Navigation').controller('MainCtrl', function ($scope, $location, SessionDataSrv) {
 
     $scope.model = {
-        userName : '',
-        countLinkedCars : 0,
+        userName: '',
+        countLinkedCars: 0,
         sywNumber: 0,
-        currentPath : $location.path(),
-        currentCar : ''
+        currentPath: $location.path(),
+        currentCar: ''
     };
 
     $scope.$on('RELOAD_VEHICLES', function (/*event, dataResponse*/) {
-        $scope.model.countLinkedCars =  SessionDataSrv.getCurrentFamilyVehicles().length;
-        $scope.model.userName =  SessionDataSrv.getCurrentUserName();
-        $scope.model.sywNumber =  SessionDataSrv.getSywMemberNumber();
+        $scope.model.countLinkedCars = SessionDataSrv.getCurrentFamilyVehicles().length;
+        $scope.model.userName = SessionDataSrv.getCurrentUserName();
+        $scope.model.sywNumber = SessionDataSrv.getSywMemberNumber();
     });
 
     $scope.$on('RELOAD_SELECTED_VEHICLE', function (/*event, dataResponse*/) {
-        $scope.model.currentCar =  SessionDataSrv.getCurrentFamilyVehicle();
+        $scope.model.currentCar = SessionDataSrv.getCurrentFamilyVehicle();
     });
 
     /**
@@ -27,27 +27,31 @@ angular.module('Navigation').controller('MainCtrl', function ($scope, $location,
         $scope.model.currentPath = $location.path();
     });
 
-    $scope.isLandingPage = function(){
+    $scope.isMobile = function () {
+        return SessionDataSrv.isMobileDevice();
+    };
+
+    $scope.isLandingPage = function () {
         return $location.path() === '/landing';
     };
-    $scope.isDashboardPage = function(){
+    $scope.isDashboardPage = function () {
         return $location.path() === '/dashboard';
     };
 
-    $scope.isCarProfilePage = function(){
+    $scope.isCarProfilePage = function () {
         return $location.path() === '/car-profile';
     };
 
-    $scope.isServicesOrRecallsPage = function(){
+    $scope.isServicesOrRecallsPage = function () {
         return $location.path() === '/recalls' || $location.path() === '/services';
     };
 
-    $scope.isFindServicePage = function(){
+    $scope.isFindServicePage = function () {
         return $location.path() === '/store-locator';
     };
 
-    $scope.getSywNumber = function(){
-        if($scope.model.sywNumber){
+    $scope.getSywNumber = function () {
+        if ($scope.model.sywNumber) {
             return $scope.model.sywNumber.substring(1, $scope.model.sywNumber.length - 1);
         }
         return '';
