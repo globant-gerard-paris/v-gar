3d81fac91ac870885af128d9d0893cb516c10c31
Guillermo Baigorria <g.baigorria@globant.com>
2014-12-18 16:54:29 -0300

    fixed carousel

--- FE/app/scripts/directives/dashboard/slick.js
+++ FE/app/scripts/directives/dashboard/slick.js
@@ -1,47 +1,49 @@
 'use strict';
 
-angular.module('Directives').directive('slick', function () {
+angular.module('Directives').directive('slick', function ($timeout) {
     return {
         restrict: 'A',
         scope: {
             data: '=slickData'
         },
         link: function(scope, el) {
+
             scope.$watch('data', function(val){
                 if(!angular.isArray(val) || val.length === 0){
                     return;
                 }
 
                 // remove slick if data changed
-                el.unslick();
+                if(scope.initialized){
+                    el.unslick();
+                    scope.initialized = false;
+                }
 
-                //... this is a bug, does not remove this element and generate others children on this one,
-                // this snippet remove the previous one.
-                var stickList = el.context.getElementsByClassName('slick-list')[0];
-                if(stickList){stickList.remove();}
-
-                // (re|)init
-                el.slick({
-                    dots: false,
-                    infinite: false,
-                    autoplay: false,
-                    slidesToShow: val.length < 3 ? val.length : 3,
-                    slidesToScroll: 1,
-                    responsive: [
-                        {
-                            breakpoint: 768,
-                            settings: {
-                                slidesToShow: 1
+                scope.initialized = true;
+                $timeout( function(){
+                    // (re|)init
+                    el.slick({
+                        dots: false,
+                        infinite: false,
+                        autoplay: false,
+                        slidesToShow: val.length < 3 ? val.length : 3,
+                        slidesToScroll: 1,
+                        responsive: [
+                            {
+                                breakpoint: 768,
+                                settings: {
+                                    slidesToShow: 1
+                                }
+                            },
+                            {
+                                breakpoint: 992,
+                                settings: {
+                                    slidesToShow: val.length < 2 ? val.length : 2
+                                }
                             }
-                        },
-                        {
-                            breakpoint: 992,
-                            settings: {
-                                slidesToShow: val.length < 2 ? val.length : 2
-                            }
-                        }
-                    ]
-                });
+                        ]
+                    });
+                },500);
 
             });
         }
