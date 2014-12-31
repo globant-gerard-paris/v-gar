defcbf24b698e8d02ff8012e6bb24d15c660f546
Rammel Maestre <rammel.maestre@globant.com>
2014-12-31 14:07:48 -0200

    first fix

--- BK/src/main/java/com/searshc/mygarage/controllers/DefaultController.java
+++ BK/src/main/java/com/searshc/mygarage/controllers/DefaultController.java
@@ -4,7 +4,6 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,21 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author rammel.maestre
  */
 @Controller
-public class DefaultController implements ErrorController {
-
-    private static final String PATH = "/error";
+public class DefaultController {
 
     private static final Log log = LogFactory.getLog(DefaultController.class);
 
-    @RequestMapping(value = PATH)
-    public String error(HttpServletRequest request) {
+    @RequestMapping(value = {"/dashboard", "/car-profile", "/store-locator", "/recalls", "/services"})
+    public String redirect(HttpServletRequest request) {
         log.warn("Not found: " + request.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI));
         return "redirect:/";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
     }
 
 }
